2b8433207c4178be30571893764ed21891414aa3
Gabriele Della Casa Venturelli <hardsetting@gmail.com>
2020-06-01 19:06:27 -0700

    Replaced all zip dependencies with tar.gz

--- conanfile.py
+++ conanfile.py
@@ -15,7 +15,7 @@ class TFLiteConan(ConanFile):
     license = "Apache-2.0"
 
     # Conan build process settings
-    exports = ['01-fix-missing-sources.patch']
+    exports = ["01-fix-missing-sources.patch", "02-tar-instead-of-zip.patch"]
     settings = "os", "arch", "compiler", "build_type"
 
     build_subfolder = "tensorflow/lite/tools/make"
@@ -25,6 +25,7 @@ class TFLiteConan(ConanFile):
 
     def source(self):
         tools.patch(patch_file="01-fix-missing-sources.patch")
+        tools.patch(patch_file="02-tar-instead-of-zip.patch")
         self.run(f"{self.build_subfolder}/download_dependencies.sh")
 
     def build(self):
